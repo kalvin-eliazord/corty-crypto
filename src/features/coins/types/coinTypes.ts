dfb3e6d4f8d5cd79b346ce0c61319f242941e776
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-08 00:41:12 +0200

    added tableHeaders type

--- src/features/coins/types/coinTypes.ts
+++ src/features/coins/types/coinTypes.ts
@@ -58,3 +58,5 @@ export type MarketChart = {
   day: string;
   amount: number;
 };
+
+
---
+++ src/features/coins/types/tableCoins.ts
@@ -0,0 +1,12 @@
+export const tableHeaders = [
+    "#",
+    "Name",
+    "Price",
+    "1h",
+    "24h",
+    "7d",
+    "24h Vol / Market Cap",
+    "Circulating / Total Sup",
+    "Last 7d",
+  ];
+  
