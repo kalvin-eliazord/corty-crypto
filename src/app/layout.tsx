76d971fe77d71cdacbca3d1fa2a8ec11efd2fc71
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-10 22:32:25 +0200

    implemented my convertor component into my layout and moved navbarLinks data struct into the layout file for more clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { navbarLinks } from "./configs/navbarLinks";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 import { SearchCoins } from "@/features/coins/components/SearchCoins";
+import { Convertor } from "@/features/convertor/components/Convertor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +16,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const navbarLinks = [
+  { name: "Home", path: "/" },
+  { name: "Portfolio", path: "/portfolio" },
+];
 
 export default function RootLayout({
   children,
@@ -34,6 +39,7 @@ export default function RootLayout({
                 <Link href={link.path}> {link.name}</Link>
               </li>
             ))}
+            <Convertor />
             <SearchCoins />
           </ul>
           {children}
