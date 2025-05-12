39c59c585dfae3e9c1a99891b1c6ac0934bcbd3d
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-12 23:45:56 +0200

    added tailwind and shadecn packages

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        muted: "#e5e7eb",
+        "muted-dark": "#1f2937",
+      },
+    },
+  },
+  plugins: [],
+};
