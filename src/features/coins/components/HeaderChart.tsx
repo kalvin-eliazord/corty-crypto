338ac356bc2085a8e1dc0b54a60d317ee33b2505
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-14 23:27:16 +0200

    refactored HeaderCharts

--- src/features/coins/components/HeaderChart.tsx
+++ src/features/coins/components/HeaderChart.tsx
@@ -1,13 +1,30 @@
 import { format } from "date-fns";
 import { HeaderProps } from "../types/coinTypes";
+import { convertAmount } from "@/features/convertor/utils/convertAmount";
+import { formatAmount } from "@/features/convertor/utils/formatAmount";
+import Image from "next/image";
 
-export const HeaderChart: React.FC<HeaderProps> = ({ name, marketChart }) => {
+export const HeaderChart: React.FC<HeaderProps> = ({
+  name,
+  marketChart,
+  currencyInfo,
+  coin,
+}) => {
   const todayDate = format(new Date(), "MMMM d, yyyy");
+  const convertedAmount = convertAmount(marketChart.amount, currencyInfo.value);
 
   return (
     <div>
-      <div>{name}</div>
-      <div>{`currency ${marketChart.amount} amount`}</div>
+      {coin ? (
+        <div className="flex">
+          <Image src={coin.image} width={96} height={96} alt={"coin logo"} />(
+          {name})
+        </div>
+      ) : (
+        <div> {name} </div>
+      )}
+
+      <div>{`${currencyInfo.unit} ${formatAmount(convertedAmount)} `}</div>
       <div>{todayDate}</div>
     </div>
   );
