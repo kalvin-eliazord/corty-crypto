3545735c9f16053f8c151cf4ac81558707ee9224
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-14 23:26:04 +0200

    refactored charts

--- src/features/coins/components/Charts.tsx
+++ src/features/coins/components/Charts.tsx
@@ -1,13 +1,36 @@
 import { useCoinChart } from "../hooks/useCoinChart";
 import { PriceChart } from "@/features/coins/components/PriceChart";
 import { VolumeChart } from "@/features/coins/components/VolumeChart";
+import { CurrencyInfo } from "@/features/convertor/types/currency";
+import { CoinType } from "../types/coinTypes";
 
-export const Charts = ({ coinId }: { coinId: string }) => {
+type ChartsProps = {
+  coinId: string;
+  currencyInfo: CurrencyInfo;
+  coin: CoinType | undefined;
+};
+
+export const Charts: React.FC<ChartsProps> = ({
+  coinId,
+  currencyInfo,
+  coin,
+}) => {
   const { data, status, error } = useCoinChart(coinId);
   return (
     <div className="flex gap-x-4 rounded-xl p-6 shadow-lg dark:bg-slate-800">
-      <PriceChart data={data} status={status} error={error}></PriceChart>
-      <VolumeChart data={data} status={status} error={error}></VolumeChart>
+      <PriceChart
+        data={data}
+        status={status}
+        error={error}
+        coin={coin}
+        currencyInfo={currencyInfo}
+      />
+      <VolumeChart
+        data={data}
+        status={status}
+        error={error}
+        currencyInfo={currencyInfo}
+      />
     </div>
   );
 };
