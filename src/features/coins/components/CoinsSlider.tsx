b4ff2281fd7382091ab7fdafccbe4e29abcf08ab
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-14 23:26:40 +0200

    refactored coinsSlider

--- src/features/coins/components/CoinsSlider.tsx
+++ src/features/coins/components/CoinsSlider.tsx
@@ -1,5 +1,7 @@
-
+import { convertAmount } from "@/features/convertor/utils/convertAmount";
 import { CoinsSliderProps, CoinType } from "../types/coinTypes";
+import { formatAmount } from "@/features/convertor/utils/formatAmount";
+import Image from "next/image";
 
 export const CoinsSlider: React.FC<CoinsSliderProps> = ({
   allCoins,
@@ -7,6 +9,7 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
   error,
   coinId,
   setCoinId,
+  currencyInfo,
 }: CoinsSliderProps) => {
   if (status === "rejected") {
     return <>hasError : {error}</>;
@@ -17,7 +20,7 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
   }
 
   return (
-    <ul className="flex gap-x-4 rounded-xl p-6 shadow-lg dark:bg-slate-800">
+    <ul className="flex gap-x-20 rounded-xl p-6 shadow-lg dark:bg-slate-800">
       {allCoins &&
         allCoins.slice(0, 10).map((coin: CoinType) => (
           <li
@@ -29,7 +32,24 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
             }
             onClick={() => setCoinId(coin.id)}
           >
-            {coin.name}
+            <div className="flex  rounded-xl  dark:bg-slate-800">
+              <Image
+                src={coin.image}
+                alt={"coin logo"}
+                width={96}
+                height={96}
+              />
+              <div className="flex gap-x-4 rounded-xl p-6 shadow-lg dark:bg-slate-800">
+                {coin.name}({coin.symbol.toUpperCase()})
+              </div>
+              <div className="flex gap-x-4 rounded-xl p-6 shadow-lg dark:bg-slate-800">
+                {formatAmount(
+                  convertAmount(coin.current_price, currencyInfo?.value)
+                )}
+                {currencyInfo?.unit}
+                {coin.price_change_percentage_1h_in_currency.toFixed(2)}%
+              </div>
+            </div>
           </li>
         ))}
     </ul>
