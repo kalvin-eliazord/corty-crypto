e0afe114e00594c6ff7a0c43a68ba06c2823fd66
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-14 22:55:48 +0200

    added navbar component and new css font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,16 @@
 "use client";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { Provider } from "react-redux";
 import { store } from "@/store";
-import { SearchCoins } from "@/features/coins/components/SearchCoins";
-import { Convertor } from "@/features/convertor/components/Convertor";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Navbar } from "@/shared/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const navbarLinks = [
-  { name: "Home", path: "/" },
-  { name: "Portfolio", path: "/portfolio" },
-];
 
 export default function RootLayout({
   children,
@@ -28,21 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Provider store={store}>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${spaceGrotesk.className} antialiased min-h-screen bg-gradient-to-r from-orange-900 via-purple-900 via-blue-900 to-indigo-900 `}
         >
-          <ul>
-            {navbarLinks.map((link) => (
-              <li key={link.path}>
-                <Link href={link.path}> {link.name}</Link>
-              </li>
-            ))}
-            <Convertor />
-            <SearchCoins />
-          </ul>
-          {children}
+          <div className="h-full bg-[linear-gradient(to_bottom,transparent_0%,rgba(0,0,0,0.5)_7%,black_100%)]">
+            <ThemeProvider>
+              <Navbar />
+              {children}
+            </ThemeProvider>
+          </div>
         </body>
       </Provider>
     </html>
