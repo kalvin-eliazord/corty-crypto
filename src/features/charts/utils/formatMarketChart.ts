f28d9681ac9a0989586472e02ba336d0b417e370
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-19 21:52:41 +0200

    made util func more robust

--- src/features/charts/utils/formatMarketChart.ts
+++ src/features/charts/utils/formatMarketChart.ts
@@ -2,13 +2,12 @@ import { format } from "date-fns";
 import { MarketChart, TimeAndAmount } from "../types/charts";
 
 export const formatMarketChart = (
-  allTimeAndAmounts: TimeAndAmount[]
+  allTimeAndAmounts: TimeAndAmount[] = []
 ): MarketChart[] => {
-  if (!allTimeAndAmounts) return [];
+  if (allTimeAndAmounts.length === 0) return [];
 
-  return allTimeAndAmounts.slice(90).map(([timestamp, amount]) => ({
+  return allTimeAndAmounts.slice(-30).map(([timestamp, amount]) => ({
     day: format(new Date(timestamp), "dd"),
     amount,
   }));
 };
- 
