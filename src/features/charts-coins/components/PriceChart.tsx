3266711c69f4d38a71266d61aa6e3396d271821a
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-19 07:18:55 +0200

    refactored price chart

--- src/features/charts-coins/components/PriceChart.tsx
+++ src/features/charts-coins/components/PriceChart.tsx
@@ -1,9 +1,15 @@
 import { Line, XAxis, AreaChart, Area } from "recharts";
-import { ChartProps } from "../types/coinTypes";
 import { HeaderChart } from "./HeaderChart";
 import { formatMarketChart } from "../utils/formatMarketChart";
+import { ChartProps } from "../types/charts";
 
-export const PriceChart: React.FC<ChartProps> = ({ data, status, error }) => {
+export const PriceChart: React.FC<ChartProps> = ({
+  data,
+  status,
+  error,
+  currencyInfo,
+  coin,
+}) => {
   if (status === "rejected" && error) {
     return <>Error : {error}</>;
   }
@@ -18,8 +24,10 @@ export const PriceChart: React.FC<ChartProps> = ({ data, status, error }) => {
     <div>
       {prices && (
         <HeaderChart
-          name={"Price"}
+          name={coin?.symbol.toUpperCase()}
           marketChart={prices[prices.length - 1]}
+          currencyInfo={currencyInfo}
+          coin={coin}
         ></HeaderChart>
       )}
 
