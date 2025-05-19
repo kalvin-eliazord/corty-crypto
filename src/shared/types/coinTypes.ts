68dbc862a960ea2510bb21ddf0d4b0dc231a1f87
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-19 07:56:28 +0200

    moved files to shared folder

--- src/shared/types/coinTypes.ts
+++ src/shared/types/coinTypes.ts
@@ -1,9 +1,13 @@
+import { CurrencyInfo } from "@/features/currency-selector/types/currencyInfo";
+
 export type FetchStatus = "pending" | "fulfilled" | "rejected";
 
-export type CoinsState = {
+export type AllCoinsProps = {
   allCoins: CoinType[];
   status: FetchStatus;
   error: string | undefined | null;
+  currencyInfo: CurrencyInfo | null;
+  coin?: CoinType;
 };
 
 export type CoinType = {
@@ -28,36 +32,4 @@ export type CoinType = {
   price_change_percentage_1h_in_currency: number;
   price_change_percentage_24h_in_currency: number;
   price_change_percentage_7d_in_currency: number;
-};
-
-export type CoinsSliderProps = {
-  coinId: string;
-  setCoinId(coinId: string): void;
-  allCoins: CoinType[];
-  status: FetchStatus;
-  error: string | null | undefined;
-};
-
-export type TimeAndAmount = [number, number];
-
-export type MarketCharts = {
-  prices: TimeAndAmount[];
-  market_caps: TimeAndAmount[];
-  total_volumes: TimeAndAmount[];
-};
-
-export type ChartProps = {
-  data: MarketCharts | undefined;
-  status: string;
-  error: string | null | undefined;
-};
-
-export type HeaderProps = {
-  name: string;
-  marketChart: MarketChart;
-};
-
-export type MarketChart = {
-  day: string;
-  amount: number;
 };
