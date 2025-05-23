cc05dc446a9ea01be07fce772dae58b0ad122c2c
kalvin-eliazord <kalvineliazord@gmail.com>
2025-05-23 11:00:19 +0200

    slider coins

--- src/features/slider-coins/components/CoinsSlider.tsx
+++ src/features/slider-coins/components/CoinsSlider.tsx
@@ -1,6 +1,5 @@
-import { AllCoinsProps, CoinType } from "@/shared/types/coinTypes";
-import { convertAmount } from "@/shared/utils/convertAmount";
-import { formatAmount } from "@/shared/utils/formatAmount";
+import { AllCoinsProps, CoinType } from "@/shared/types/coins";
+//import { formatAmount } from "@/shared/utils/formatAmount";
 import Image from "next/image";
 import {
   Carousel,
@@ -10,6 +9,8 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { OneHourPercentage } from "@/shared/components/OneHourPercentage";
+import { formatAmount } from "@/shared/utils/formatAmount";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type CoinsSliderProps = AllCoinsProps & {
   coinId: string;
@@ -22,14 +23,25 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
   error,
   coinId,
   setCoinId,
-  currencyInfo,
+  currency,
 }: CoinsSliderProps) => {
   if (status === "rejected") {
-    return <>hasError : {error}</>;
+    return (
+      <div className="flex-1 dark:bg-[#1F1D2280] p-5 rounded-xl border-t border-l border-r w-full text-center">
+        {" "}
+        Coins Slider fetching rejected : {error}
+      </div>
+    );
   }
 
   if (status === "pending") {
-    return <>isLoading</>;
+    return (
+      <div className="w-full flex gap-x-8 p-3">
+        {Array.from({ length: 10 }, (_, i) => (
+          <Skeleton className="h-12 w-full rounded mr-8" key={i} />
+        ))}
+      </div>
+    );
   }
 
   return (
@@ -46,7 +58,7 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
                 <div
                   className={
                     coinId === coin.id
-                      ? "flex items-center gap-3 px-4 py-2 bg-[#1E1D23] rounded-lg hover:cursor-pointer"
+                      ? "flex items-center gap-3 px-4 py-2 bg-[#1E1D23] border-t rounded-lg hover:cursor-pointer"
                       : "flex items-center gap-3 px-4 py-2 bg-[#1F1D2280] rounded-lg hover:cursor-pointer"
                   }
                   onClick={() => setCoinId(coin.id)}
@@ -64,13 +76,15 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
                         {coin.name} ({coin.symbol.toUpperCase()})
                       </span>
                       <div className="flex gap-2 text-gray-400">
-                        {formatAmount(
-                          convertAmount(coin.current_price, currencyInfo?.value)
-                        )}{" "}
-                        {currencyInfo?.unit}
+                        {formatAmount(coin.current_price)} {currency?.symbol}
                         <OneHourPercentage
                           percentage={
                             coin.price_change_percentage_1h_in_currency
+                          }
+                          color={
+                            coin.price_change_percentage_1h_in_currency > 0
+                              ? "#00F5E4"
+                              : "#FF0061"
                           }
                         />
                       </div>
