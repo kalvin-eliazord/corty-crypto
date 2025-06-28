c10169e5b89fb489de948021f108901713f1979c
kalvin-eliazord <kalvineliazord@gmail.com>
2025-06-29 01:37:28 +0200

    9- added coins slider feature

--- src/features/slider-coins/components/CoinsSlider.tsx
+++ src/features/slider-coins/components/CoinsSlider.tsx
@@ -1,7 +1,4 @@
-import { AllCoinsProps, CoinType } from "@/shared/types/coinTypes";
-import { convertAmount } from "@/shared/utils/convertAmount";
-import { formatAmount } from "@/shared/utils/formatAmount";
-import Image from "next/image";
+import { AllCoinsProps, CoinType } from "@/shared/types/coins";
 import {
   Carousel,
   CarouselContent,
@@ -9,7 +6,9 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { OneHourPercentage } from "@/shared/components/OneHourPercentage";
+import { Skeleton } from "@/components/ui/skeleton";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { Coin } from "./Coin";
 
 type CoinsSliderProps = AllCoinsProps & {
   coinId: string;
@@ -18,18 +17,19 @@ type CoinsSliderProps = AllCoinsProps & {
 
 export const CoinsSlider: React.FC<CoinsSliderProps> = ({
   allCoins,
-  status,
-  error,
   coinId,
   setCoinId,
-  currencyInfo,
+  currency,
+  isLoading,
 }: CoinsSliderProps) => {
-  if (status === "rejected") {
-    return <>hasError : {error}</>;
-  }
-
-  if (status === "pending") {
-    return <>isLoading</>;
+  if (isLoading) {
+    return (
+      <div className="w-full flex gap-x-8 p-3">
+        {Array.from({ length: 10 }, (_, i) => (
+          <Skeleton className="h-12 w-full rounded mr-8" key={i} />
+        ))}
+      </div>
+    );
   }
 
   return (
@@ -43,42 +43,25 @@ export const CoinsSlider: React.FC<CoinsSliderProps> = ({
           {allCoins &&
             allCoins.map((coin: CoinType) => (
               <CarouselItem key={coin.id} className="basis-auto">
-                <div
-                  className={
-                    coinId === coin.id
-                      ? "flex items-center gap-3 px-4 py-2 bg-gray-900 rounded-lg hover:cursor-pointer"
-                      : "flex items-center gap-3 px-4 py-2 bg-gray-800 rounded-lg hover:cursor-pointer"
-                  }
-                  onClick={() => setCoinId(coin.id)}
-                >
-                  <div className="flex items-center gap-3">
-                    <Image
-                      src={coin.image}
-                      alt={`${coin.name} logo`}
-                      width={32}
-                      height={32}
-                      className="w-8 h-8 rounded-full"
+                {coinId === coin.id ? (
+                  <BackgroundGradient rounded="lg" isBlurred={false}>
+                    <Coin
+                      coinId={coinId}
+                      setCoinId={setCoinId}
+                      coin={coin}
+                      className="flex items-center gap-3 px-4 py-[7px] dark:bg-[#1E1D23] dark:border-t rounded-lg hover:cursor-pointer "
+                      currency={currency}
                     />
-                    <div className="flex-1">
-                      <span className="text-gray-200 font-medium">
-                        {coin.name} ({coin.symbol.toUpperCase()})
-                      </span>
-                      <div className="flex  gap-2 text-gray-400">
-                        {formatAmount(
-                          convertAmount(coin.current_price, currencyInfo?.value)
-                        )}{" "}
-                        {currencyInfo?.unit}
-                        <OneHourPercentage
-                          percentage={
-                            coin.price_change_percentage_1h_in_currency
-                          }
-                        />
-                      </div>
-                    </div>
-
-                    <div className="flex items-center gap-1"></div>
-                  </div>
-                </div>
+                  </BackgroundGradient>
+                ) : (
+                  <Coin
+                    coinId={coinId}
+                    coin={coin}
+                    setCoinId={setCoinId}
+                    className="flex items-center gap-3 px-4 py-2 dark:bg-[#1F1D2280] bg-gray-700 rounded-lg hover:cursor-pointer"
+                    currency={currency}
+                  />
+                )}
               </CarouselItem>
             ))}
         </CarouselContent>
