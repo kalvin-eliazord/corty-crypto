c99c1d66661da1fbd450072b0b1f267f62b26894
kalvin-eliazord <kalvineliazord@gmail.com>
2025-06-29 13:37:10 +0200

    installed next themes and added tailwind config

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        muted: "#e5e7eb",
+      },
+    },
+  },
+};
