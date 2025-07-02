ca7efc7bca2e23978777cddb70039d290471a4c5
kalvin-eliazord <kalvineliazord@gmail.com>
2025-07-02 20:10:35 +0200

    implemented safety check to not fetch if the portfolio is empty

--- src/features/portfolio/utils/removeDuplicates.ts
+++ src/features/portfolio/utils/removeDuplicates.ts
@@ -1,6 +1,8 @@
 import { PortfolioType, UniqueAsset } from "../types/portfolio";
 
-export const removeDuplicates = (portfolio: PortfolioType): UniqueAsset => {
+export const removeDuplicates = (portfolio: PortfolioType): UniqueAsset  | null=> {
+  if(!portfolio || portfolio.length === 0 ) return null;
+
   return portfolio.reduce((acc, el) => {
     if (acc[el.id]) return acc;
 
